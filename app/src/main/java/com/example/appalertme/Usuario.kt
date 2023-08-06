e78b798b00f1cd8840e860371929fcf49f252cb5
ham0ncito <106882518+ham0ncito@users.noreply.github.com>
2023-08-06 11:52:26 -0600

    funcionaaaa

---
+++ app/src/main/java/com/example/appalertme/Usuario.kt
@@ -0,0 +1,11 @@
+package com.example.appalertme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Usuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_usuario)
+    }
+}
--- app/src/main/java/com/example/appalertme/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/appalertme/ui/home/HomeFragment.kt
@@ -1,21 +1,29 @@
 package com.example.appalertme.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.appalertme.R
+import com.example.appalertme.Usuario
 import com.example.appalertme.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
+        val roundedImageView = view.findViewById<ImageView>(R.id.roundedImageView)
+        roundedImageView.setOnClickListener {
+            val intent = Intent(requireContext(), Usuario::class.java)
+            startActivity(intent)
+    }}
 }
